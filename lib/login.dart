679357f72a3241d8a89d9c76d933cf648450f754
MAMuhajir <61780789+MAMuhajir@users.noreply.github.com>
2023-01-29 19:26:40 +0800

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/src/provider.dart';
 import 'package:rhb/appointment.dart';
 
 class Login extends StatefulWidget {
@@ -81,9 +80,6 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
                             Buttons.GoogleDark,
                             onPressed: () {
                               // Do google auth here'
-                              final provider = 
-                                  Provider.of<GoogleSignInProvider>(context, listen:false);
-                              provider.googleLogin();
                               Navigator.of(context).push(
                                 MaterialPageRoute(
                                   builder: (BuildContext context) {
